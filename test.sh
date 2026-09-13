ae6fbadf5f5a49a5632f700f5faec0f0a7d91f8f
Koji Murata <k@malt03.com>
2026-09-14 03:40:57 +0900

    Cover lexical Self match payloads across generic instances

--- test.sh
+++ test.sh
@@ -370,6 +370,11 @@ if sh ./test-mid-record-construction.sh; then
 else
     fail=$((fail + 1)); failed="$failed mid-record-construction"
 fi
+if sh ./test-mid-self-layout.sh; then
+    :
+else
+    fail=$((fail + 1)); failed="$failed mid-self-layout"
+fi
 if sh ./test-mid-field-defaults.sh; then
     :
 else
